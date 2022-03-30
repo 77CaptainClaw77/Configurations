237ffd396347ec6e674c56c10526577bede45a71
77CaptainClaw77 <kumar1235813@gmail.com>
2022-03-31 03:21:02 +0530

    Added vim configs, evil mode

--- init.el
+++ init.el
@@ -92,7 +92,7 @@
    '("9685cefcb4efd32520b899a34925c476e7920725c8d1f660e7336f37d6d95764" "730a87ed3dc2bf318f3ea3626ce21fb054cd3a1471dcd59c81a4071df02cb601" "a3b6a3708c6692674196266aad1cb19188a6da7b4f961e1369a68f06577afa16" "3df5335c36b40e417fec0392532c1b82b79114a05d5ade62cfe3de63a59bc5c6" "01cf34eca93938925143f402c2e6141f03abb341f27d1c2dba3d50af9357ce70" "4f01c1df1d203787560a67c1b295423174fd49934deb5e6789abd1e61dba9552" "2cdc13ef8c76a22daa0f46370011f54e79bae00d5736340a5ddfe656a767fddf" "54cf3f8314ce89c4d7e20ae52f7ff0739efb458f4326a2ca075bf34bc0b4f499" "7b3d184d2955990e4df1162aeff6bfb4e1c3e822368f0359e15e2974235d9fa8" "6c3b5f4391572c4176908bb30eddc1718344b8eaff50e162e36f271f6de015ca" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "77bd459212c0176bdf63c1904c4ba20fce015f730f0343776a1a14432de80990" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "3a78cae35163bb71df460ebcfdebf811fd7bc74eaa15428c7e0bccfd4f858d30" "7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" default))
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(chess centaur-tabs lua-mode magit sed-mode multi-term company which-key treemacs treemacs-all-the-icons page-break-lines dashboard smart-mode-line smart-mode-line-atom-one-dark-theme smart-mode-line-powerline-theme cherry-blossom-theme cyberpunk-theme darkokai-theme grandshell-theme hemisu-theme monokai-theme doom all-the-icons doom-modeline doom-themes yasnippet-snippets auto-complete-c-headers))
+   '(evil chess centaur-tabs lua-mode magit sed-mode multi-term company which-key treemacs treemacs-all-the-icons page-break-lines dashboard smart-mode-line smart-mode-line-atom-one-dark-theme smart-mode-line-powerline-theme cherry-blossom-theme cyberpunk-theme darkokai-theme grandshell-theme hemisu-theme monokai-theme doom all-the-icons doom-modeline doom-themes yasnippet-snippets auto-complete-c-headers))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -140,4 +140,4 @@
 ;; enable windmove with meta-left, meta-right ,etc
 (windmove-default-keybindings 'meta)
 
- 
+(evil-mode t)
